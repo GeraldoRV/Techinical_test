7b9cfbe77f5645dcd33823a0403edba42fdc0527
LAPTOP-09QGLT5H\Geraldo <geraldo.rodriguesveloso111@gmail.com>
2019-12-05 18:56:55 +0000

    Added table with users

--- FrontendBackend/frontend/src/app/app.module.ts
+++ FrontendBackend/frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgbdSortableHeader, UserListComponent} from './component/user-list/user-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes = [
+  {path: '', component: UserListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdSortableHeader,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ FrontendBackend/frontend/src/app/component/user-list/user-list.component.ts
@@ -0,0 +1,75 @@
+import {Component, Directive, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+interface User {
+  id: number;
+  name: string;
+  roles: string[];
+}
+
+const USERS: User[] = [
+  {
+    id: 1,
+    name: 'Felipe',
+    roles: ['standard', 'admin']
+  }, {
+    id: 2,
+    name: 'Roberto',
+    roles: ['standard']
+  }, {
+    id: 3,
+    name: 'Maria',
+    roles: ['standard']
+  }, {
+    id: 4,
+    name: 'Rebeca',
+    roles: ['standard']
+  }
+];
+
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: { [key: string]: SortDirection } = {'asc': 'desc', 'desc': '', '': 'asc'};
+export const compare = (v1, v2) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: string;
+  direction: SortDirection;
+}
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: 'th[sortable]',
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[class.asc]': 'direction === "asc"',
+    '[class.desc]': 'direction === "desc"',
+    '(click)': 'rotate()'
+  }
+})
+// tslint:disable-next-line:directive-class-suffix
+export class NgbdSortableHeader {
+
+  @Input() sortable: string;
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({column: this.sortable, direction: this.direction});
+  }
+}
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users = USERS;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
